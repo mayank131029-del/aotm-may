cbcc53038bd5994b2c3ab850c43fac847345b75a
mayank131029-del <mayank131029@gmail.com>
2026-05-08 09:46:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const logo = document.querySelector(".logo")
+
+logo.addEventListener("click", ()=>{ 
+    window.open('index.html', '_self')})
+
+
+const back2top = document.querySelector(".back2top");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        back2top.classList.add("show");
+    }
+    else{
+        back2top.classList.remove("show");
+    }
+
+});
+
+back2top.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
